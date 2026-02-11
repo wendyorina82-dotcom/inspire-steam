3aa6c3e255a4a53f1ec8cdc4e0d7a3e2dbc933d3
Wendy <wendyorina82@gmail.com>
2026-02-11 09:17:27 +0300

    first commit

---
+++ python_basics/challenge1.py
@@ -0,0 +1,8 @@
+university="JKUAT"
+grade="B-"
+favourite_course="softwaredevelopment"
+cluster_points="54"
+print(f"My favourite university is {university}")
+print(f"My favourite_course is {favourite_course}")
+print(f"My grade is {grade}")
+print(f"My cluster_points are {cluster_points}")
---
+++ python_basics/hello.py
@@ -0,0 +1,13 @@
+#This is a comment
+print("Hello from Wendy")
+#County
+print("I am from Nyamira")
+#Hobbies
+print("I like playing basketball,listening to music,dancing and cooking")
+#age,height,weight
+age=18
+height=1.67
+weight=52
+print(f"I am {age} years old")
+print(f"I am {height} metres")
+print(f"I weigh {weight} kilograms")
