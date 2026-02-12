f0744f9b6dda99f31ba487c12e7e324731320a3d
Wendy <wendyorina82@gmail.com>
2026-02-12 20:46:46 +0300

    Mpesa message

---
+++ python_basics/mpesa_message.py
@@ -0,0 +1,18 @@
+#Name:Wendy Orina
+#Date:12/02/2026
+#Mpesa message
+
+From = "Wendy Orina"
+amount_added = 600
+mpesa_balance = 1500
+mpesa_code = "QGHU126HUF"
+
+
+print("CONFIRMED")
+print(f"You have received {amount_added}KES from {From}")
+print(f"mpesa_code:{mpesa_code}")
+
+new_balance= int(amount_added) + int(mpesa_balance)
+
+print(f"Your new balance is {new_balance}KES")
+
