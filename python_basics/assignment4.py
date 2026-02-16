048201754018cec3d5f9d6f6a0102f42b52e22a7
Wendy <wendyorina82@gmail.com>
2026-02-16 22:14:27 +0300

    show loops,calculate income tax

---
+++ python_basics/assignment4.py
@@ -0,0 +1,13 @@
+#Name:Wendy Orina
+#Date:02/11/2026
+#Program to show loops
+import math
+
+for x in range(-180,180,30):
+    print(math.cos(math.radians(x)))
+
+for x in range(-180,180,30):
+    print(math.sin(math.radians(x)))
+for x in range(-180,180,30):
+    print(math.tan(math.radians(x)))
+
--- python_basics/assignment5.py
+++ python_basics/assignment5.py
@@ -8,6 +8,14 @@ if salary < 50000:
     tax = (2.5 * salary) / 100
     net_salary = salary - tax
 
-    print(f"Gross salary = {salary}")
-    print(f"Net salary = {net_salary}")
-    print(f"Tax = {tax}")
+elif salary <= 100000:
+    tax = (4.5 * salary) / 100
+    net_salary = salary - tax
+
+else:
+    tax = (7.5 * salary) / 100
+    net_salary = salary - tax
+
+print(f"Gross salary = {salary}")
+print(f"Net salary = {net_salary}")
+print(f"Tax = {tax}")
